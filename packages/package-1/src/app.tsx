b9793e8c46bc686bd3e7b40d0d3e40bbd6956388
diegom <diego@smartbr.com>
2021-06-17 18:25:29 -0300

    feat(test): teste

--- packages/package-1/src/app.tsx
+++ packages/package-1/src/app.tsx
@@ -2,6 +2,6 @@ import React from 'react';
 
 import { sum } from '@monorepo/commons';
 
-const App = () => <h1>Hello World PKG-1! result of sum: {sum(10, 10)}</h1>;
+const App = () => <h1>Hello World PKG-1, result of sum: {sum(10, 10)}</h1>;
 
 export default App;
