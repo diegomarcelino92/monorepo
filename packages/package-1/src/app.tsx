35fe9ad021fdd8af101275ef89a36014b0ebd6e8
diegom <diego@smartbr.com>
2021-07-10 20:03:37 -0300

    fix: teste

--- packages/package-1/src/app.tsx
+++ packages/package-1/src/app.tsx
@@ -1,5 +1,5 @@
 import { sum } from '@monorepo/commons';
 
-const App = () => <h1>Hello World PKG-1, result of sum: {sum(10, 10)}</h1>;
+const App = () => <h1>Hello World PKG-1 - result of sum: {sum(10, 10)}</h1>;
 
 export default App;
